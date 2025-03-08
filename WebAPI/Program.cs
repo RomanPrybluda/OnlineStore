731929be28d41d987f87dae36e0caa5b78a99042
Roman Prybluda <romanodua@gmail.com>
2025-03-08 15:11:53 +0200

    rebuild web api

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,10 +1,10 @@
 using DAL;
-using Domain;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
@@ -12,33 +12,32 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 });
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
 
-builder.Services.AddScoped<ProductService>();
+builder.Services.AddHttpClient();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddIdentityCore<AppUser>().AddRoles<IdentityRole>().AddEntityFrameworkStores<OnlineStoreDbContext>();
 
 builder.Services.AddDbContext<OnlineStoreDbContext>(options =>
 {
-    Console.WriteLine($"Connection String: {builder.Configuration.GetConnectionString("DefaultConnection")}");
-
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"),
         b => b.MigrationsAssembly("DAL"));
 });
 
+
 builder.Logging.AddConsole();
+
+
+
 
 
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
-    var dbContext = scope.ServiceProvider.GetRequiredService<OnlineStoreDbContext>();
-    if (dbContext.Database.GetPendingMigrations().Any())
-    {
-        dbContext.Database.Migrate();
-    }
+    var context = scope.ServiceProvider.GetRequiredService<OnlineStoreDbContext>();
+    context.Database.Migrate();
+
 }
 
 //if (app.Environment.IsDevelopment())
@@ -46,6 +45,8 @@ using (var scope = app.Services.CreateScope())
 app.UseSwagger();
 app.UseSwaggerUI();
 //}
+
+
 
 app.UseAuthorization();
 
