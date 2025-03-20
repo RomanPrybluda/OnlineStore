92ef63bae6651642250891818a9b62aa116008d6
Roman Prybluda <romanodua@gmail.com>
2025-03-20 07:21:57 +0200

    add Description to Category

--- Domain/Services/Category/DTO/CategoryDTO.cs
+++ Domain/Services/Category/DTO/CategoryDTO.cs
@@ -1,9 +1,23 @@
-﻿namespace Domain
+﻿using DAL;
+
+namespace Domain
 {
     public class CategoryDTO
     {
         public Guid Id { get; set; }
 
         public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public static Category ToCategory(CreateCategoryDTO request)
+        {
+            return new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Description = request.Description
+            };
+        }
     }
 }
--- Domain/Services/Category/DTO/CreateCategoryDTO.cs
+++ Domain/Services/Category/DTO/CreateCategoryDTO.cs
@@ -1,7 +1,21 @@
-﻿namespace Domain
+﻿using DAL;
+
+namespace Domain
 {
     public class CreateCategoryDTO
     {
         public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public static Category ToCategory(CreateCategoryDTO request)
+        {
+            return new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Description = request.Description,
+            };
+        }
     }
 }
