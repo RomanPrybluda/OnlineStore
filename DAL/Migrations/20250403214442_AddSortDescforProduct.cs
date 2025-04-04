c59c341b1f80d393e9fd61157e1196e1ef4f840c
Roman Prybluda <romanodua@gmail.com>
2025-04-04 09:18:33 +0300

    add short description in Product

---
+++ DAL/Migrations/20250403214442_AddSortDescforProduct.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSortDescforProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SortDescription",
+                table: "Products",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SortDescription",
+                table: "Products");
+        }
+    }
+}
--- DAL/Models/Product.cs
+++ DAL/Models/Product.cs
@@ -8,6 +8,8 @@
 
         public string Description { get; set; } = string.Empty;
 
+        public string SortDescription { get; set; } = string.Empty;
+
         public decimal Price { get; set; }
 
         public string? ImageUrl { get; set; }
