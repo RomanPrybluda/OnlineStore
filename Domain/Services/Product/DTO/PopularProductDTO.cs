9e19030ccafe02635e9b8b5f3c90974d8ae42c19
Roman Prybluda <romanodua@gmail.com>
2025-04-18 11:51:29 +0300

    add popular product DTO

---
+++ Domain/Services/Product/DTO/PopularProductDTO.cs
@@ -0,0 +1,41 @@
+﻿using DAL;
+
+namespace Domain
+{
+    public class PopularProductDTO
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public string? MainImageBaseName { get; set; }
+
+        public double Rating { get; set; }
+
+        public int Views { get; set; }
+
+        public int FavoritesCount { get; set; }
+
+        public int ReviewCount { get; set; }
+
+        public double PopularityScore { get; set; }
+
+        public static PopularProductDTO FromProduct(Product product, double popularityScore)
+        {
+            return new PopularProductDTO
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                MainImageBaseName = product.MainImageBaseName,
+                Rating = product.Rating,
+                Views = product.Views,
+                FavoritesCount = product.FavoritedByUsers.Count,
+                ReviewCount = product.Reviews.Count,
+                PopularityScore = Math.Round(popularityScore, 2)
+            };
+        }
+    }
+}
